427801f5f3c5a7234e2deaf70d5ed1589f2fba5c
FrencisAlreadyInUse <francisdclercq@gmail.com>
2017-12-05 16:46:00 +0100

    added endstate

---
+++ src/js/classes/states/End.js
@@ -0,0 +1,60 @@
+import Button from '../objects/Button';
+
+export default class End extends Phaser.State {
+  create() {
+    console.log(`[Instructions] — create()`);
+    this.createaBackground();
+    this.createChef();
+    this.createInstructions();
+    this.createWebcam();
+    this.createButton();
+  }
+
+  createaBackground() {
+    console.log(`[Menu] — createLogo()`);
+    this.background = this.add.image(0, 0, `background`);
+  }
+
+  createChef() {
+    this.chef = this.add.image(20, 105, `chef`);
+  }
+
+  createInstructions() {
+    //page title
+    this.pageTitle = this.add.text(this.world.centerX + 238, 120, `Hoera!`, {font: `90px circular-medium`, fill: `#fff`});
+    this.pageTitle.anchor.setTo(0.5, 0.5);
+
+    //text-bg
+    this.textBackground = this.game.add.graphics(0,0);
+    this.textBackground.beginFill(0xffffff, 1);
+    this.textBackground.drawRoundedRect(550, 230, 1300, 280, 5);
+    this.textBackground.anchor.setTo(0.5, 0.5);
+
+    this.instructionText = this.add.text(this.world.centerX + 200, 380,
+      `
+      Je behaalde 105 punten. Neem een foto van jezelf
+      voor op het scorebord.
+      `,
+      {font: `50px circular`, fill: `#ff780f`});
+    this.instructionText.lineSpacing = 15;
+    this.instructionText.anchor.setTo(0.5, 0.5);
+  }
+
+  createWebcam() {
+    this.webcam = this.game.add.graphics(0,0);
+    this.webcam.beginFill(0xffaa71, 1);
+    this.webcam.lineStyle(5, 0xFFFFFF, 1);
+    this.webcam.drawRoundedRect(870, 550, 650, 300, 5);
+  }
+
+  createButton() {
+    const buttonPlay = new Button(this.game, this.world.centerX + 244, this.world.height - 150, this.buttonPlayClicked, this, `button`, `Say Cheese`);
+    buttonPlay.anchor.setTo(0.5, 0.5);
+    this.add.existing(buttonPlay);
+  }
+
+  buttonPlayClicked() {
+    console.log(`[Instructions] — handleStart()`);
+    this.state.start(`Play`);
+  }
+}
