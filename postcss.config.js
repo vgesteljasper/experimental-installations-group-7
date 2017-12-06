6303d9ef56e3c63b392c5a3ddd4a1942c289bb1e
FrencisAlreadyInUse <francisdclercq@gmail.com>
2017-12-06 22:51:16 +0100

    fixed username + removed unnecessary postcss deps

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   plugins: [
-    require(`stylelint`),
-    require(`postcss-reporter`)({clearMessages: true}), // prettier stylelint reporting
-    require(`postcss-cssnext`),
-    require(`postcss-will-change`)
-  ]
+    require('stylelint'),
+    require('postcss-reporter')({ clearMessages: true }), // prettier stylelint reporting
+  ],
 };
